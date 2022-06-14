864b49efd6747b8ad6023813075635fecf888dc8
sathishkumar63 <sathishjim@gmail.com>
2022-06-14 06:35:25 +0530

    add stock-card unit test

--- libs/stock/src/lib/components/stock-card/stock-card.component.spec.ts
+++ libs/stock/src/lib/components/stock-card/stock-card.component.spec.ts
@@ -1,4 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { mockStocks } from '../../models';
 import { StockCardComponent } from './stock-card.component';
 
 describe('StockCardComponent', () => {
@@ -20,4 +27,45 @@ describe('StockCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render stocks card when the stocksList.length > 0', () => {
+    // arrange
+    component.stocksList = mockStocks;
+    // act
+    fixture.detectChanges();
+    const stockCardGroup = fixture.debugElement.query(
+      By.css('.stock-card__group')
+    ).nativeElement;
+    // assert
+    expect(stockCardGroup).toMatchSnapshot();
+  });
+
+  it('onToggle should be called when clicking the toggle', () => {
+    // arrange
+    jest.spyOn(component, 'onToggle');
+    component.stocksList = mockStocks;
+    // act
+    fixture.detectChanges();
+    const toggle = fixture.debugElement.queryAll(
+      By.css('.stock-card__item__checkbox')
+    )[0];
+    toggle.nativeElement.click();
+    fixture.detectChanges();
+    // assert
+    expect(component.onToggle).toBeCalledWith(mockStocks[0], true);
+  });
+
+  it('should emit notifyToggle event when onToggle method invoked', fakeAsync(() => {
+    // arrange
+    jest.spyOn(component.notifyToggle, 'emit');
+    const isChecked = true;
+    // act
+    component.onToggle(mockStocks[0], isChecked);
+    tick(300);
+    // assert
+    expect(component.notifyToggle.emit).toBeCalledWith({
+      ...mockStocks[0],
+      isActive: isChecked,
+    });
+  }));
 });
